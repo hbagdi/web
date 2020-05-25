0b7850e53947ddc2540fdda57c95fed7ec9b2af0
Harry Bagdi <harrybagdi@gmail.com>
2020-05-24 20:55:46 -0700

    add cname to script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -21,6 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo "hbagdi.yolo42.com" > public/CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
